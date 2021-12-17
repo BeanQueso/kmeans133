9588b758a4efd2374c70d047879c043d51ace359
BeanQueso <68991885+BeanQueso@users.noreply.github.com>
2021-12-17 15:00:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-import pandas as pd
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-import plotly.express as px
+import seaborn as sns
+import csv
 
 data = []
 
@@ -17,26 +17,56 @@ with open("new_merged.csv", 'r') as f:
 star_data_rows = data[1:]
 
 for star_data in star_data_rows:
-    solar_mass.append(star_data[2])
-    solar_radius.append(star_data[3])
-    solar_gravity.append(star_data[8])
+    solar_mass.append(star_data[6])
+    solar_radius.append(star_data[7])
 
 solar_mass.sort()
-solar_radius.sort()
-solar_gravity.sort()
+
+new_mass = []
+new_radius = []
+
+for star_data in solar_radius:
+    star_data.replace("''", '')
+    star_data.replace(',', '')
+    if '-' in star_data == True:
+        val1 = star_data.split("–")[0]
+        val2 = star_data.split("–")[1]
+        finalv = float(val1) - float(val2)
+        new_radius.append(finalv)
+    else:
+        new_radius.append(star_data)
 
 
-plt.scatter(solar_mass, solar_radius)
-plt.title('Mass, Radius')
-plt.xlabel('Mass')
-plt.ylabel('Radius')
+for star_data in solar_mass:
+    star_data.replace("''", '')
+    star_data.replace(',', '')
+    if '-' in star_data == True:
+        val1 = star_data.split("–")[0]
+        val2 = star_data.split("–")[1]
+        finalv = float(val1) - float(val2)
+        new_mass.append(finalv)
+    else:
+        new_mass.append(star_data)
 
-plt.show()
 
-plt.scatter(solar_mass, solar_gravity)
-plt.title('Mass, Gravity')
-plt.xlabel('Mass')
-plt.ylabel('Gravity')
 
-plt.show()
+X = []
 
+for index, star_data in enumerate(new_mass):
+  temp_list = [new_radius[index], star_data]
+  X.append(temp_list)
+
+wcss = []
+
+for i in range(1,11):
+  kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+  kmeans.fit(X)
+  wcss.append(kmeans.inertia_)
+
+plt.figure(figsize = (10,5))
+
+sns.lineplot(range(1,11), wcss, marker = 'o', color = 'red')
+plt.title('The elbow method')
+plt.xlabel('number of clusters')
+plt.ylabel('wcss')
+plt.show() 
